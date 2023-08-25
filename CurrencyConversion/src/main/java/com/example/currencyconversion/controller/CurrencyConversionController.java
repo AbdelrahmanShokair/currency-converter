e0bc524124bb00ebbab2d2b23a3134b7e1240aea
ktayyeb <khaledwaleed180@gmail.com>
2023-08-26 01:08:06 +0300

    Revert "Merge branch 'origin-backend' of https://github.com/AbdelrahmanShokair/currency-converter into origin-backend"
    This reverts commit 25623fc880382a80d56c60a781b82ac6dce36048, reversing
    changes made to ac2ab8c6e8a6338524ed6a3b5d1d68b6ff00bfd1.

--- CurrencyConversion/src/main/java/com/example/currencyconversion/controller/CurrencyConversionController.java
+++ CurrencyConversion/src/main/java/com/example/currencyconversion/controller/CurrencyConversionController.java
@@ -1,14 +1,12 @@
 package com.example.currencyconversion.controller;
 
 import com.example.currencyconversion.dto.ConversionDto;
-import com.example.currencyconversion.dto.CurrencyResponseDto;
+import com.example.currencyconversion.dto.CurrencyDto;
 import com.example.currencyconversion.dto.RateDto;
 import com.example.currencyconversion.service.CurrencyConversionService;
-import com.example.currencyconversion.validation.annotation.ValidCurrency;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +15,18 @@ import java.util.List;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/currency-conversions")
-@Validated
 public class CurrencyConversionController {
     private final CurrencyConversionService currencyConversionService;
     @GetMapping("/convert")
-    public ResponseEntity<ConversionDto> convert(@RequestParam @ValidCurrency String from,
-                                                 @RequestParam @ValidCurrency String to,
+    public ResponseEntity<ConversionDto> convert(@RequestParam String from,
+                                                 @RequestParam String to,
                                                  @RequestParam double amount){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(currencyConversionService.convert(from, to, amount));
-
-
     }
 
     @GetMapping("/compare")
-    public ResponseEntity<List<ConversionDto>> getRate(@RequestParam @ValidCurrency String from,
+    public ResponseEntity<List<ConversionDto>> getRate(@RequestParam String from,
                                                        @RequestParam List<String> to,
                                                        @RequestParam double amount){
 
@@ -40,15 +35,16 @@ public class CurrencyConversionController {
     }
 
     @GetMapping("/currencies")
-    public ResponseEntity<List<CurrencyResponseDto>> getAllCurrencies(){
+    public ResponseEntity<List<CurrencyDto>> getAllCurrencies(){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(currencyConversionService.getAllCurrencies());
     }
 
+    
     @GetMapping("/rates")
-    public ResponseEntity<List<RateDto>> getAllRates(@RequestParam @ValidCurrency String from,
-                                                     @RequestParam List<String> to){
+    public ResponseEntity<List<RateDto>> getAllRates(@RequestParam String from, @RequestParam List<String> to){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(currencyConversionService.getAllRates(from,to));
     }
+
 }
