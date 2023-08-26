0fd17a1072db43109b711cde6dd25e3d72e5136a
ktayyeb <khaledwaleed180@gmail.com>
2023-08-26 18:13:37 +0300

    Revert "Revert "Revert "Revert "Merge branch 'origin-backend' of https://github.com/AbdelrahmanShokair/currency-converter into origin-backend""""
    This reverts commit 9516e5e203fa10e8e7c37b0de7bc5b657edd2653.

--- CurrencyConversion/src/main/java/com/example/currencyconversion/service/CurrencyConversionService.java
+++ CurrencyConversion/src/main/java/com/example/currencyconversion/service/CurrencyConversionService.java
@@ -1,7 +1,7 @@
 package com.example.currencyconversion.service;
 
 import com.example.currencyconversion.dto.ConversionDto;
-import com.example.currencyconversion.dto.CurrencyDto;
+import com.example.currencyconversion.dto.CurrencyResponseDto;
 import com.example.currencyconversion.dto.RateDto;
 
 import java.util.List;
@@ -9,7 +9,7 @@ import java.util.List;
 public interface CurrencyConversionService {
     ConversionDto convert(String from,String to, double amount);
     List<ConversionDto> compare(String from,List<String> to, double amount);
-    List<CurrencyDto> getAllCurrencies();
+    List<CurrencyResponseDto> getAllCurrencies();
 
     List<RateDto> getAllRates(String from, List<String> to);
 }
